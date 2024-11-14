387f3b866183dd7d067defea97aeb4d232406745
crabin <1528392308@qq.com>
2024-11-14 15:37:30 +0900

    Add support for MySQL database in Django.

--- LostItemproject/LostItemproject/settings.py
+++ LostItemproject/LostItemproject/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'LostItemproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',  # database name
+        'HOST': '127.0.0.1',  # mysql host
+        'PORT': 3306,  # mysql port
+        'USER': 'root',  # mysql username
+        'PASSWORD': '123456'  # mysql password
     }
 }
 
