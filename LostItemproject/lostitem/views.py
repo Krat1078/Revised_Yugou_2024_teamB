6f6d432a2ad7fae3c71fb68958a9901b8c5f58b1
LPBin <82026062+crabin@users.noreply.github.com>
2024-11-28 18:05:35 +0900

    忘れ物ログイン機能は一部実装されていますが、画像アップロード機能は実装されていません。

--- LostItemproject/lostitem/views.py
+++ LostItemproject/lostitem/views.py
@@ -1,7 +1,9 @@
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
+from django.apps import apps
 
 from .forms import (
     UserRegisterForm,
@@ -31,3 +33,48 @@ class SignUpView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('lostitem:login')
     template_name = 'users/signup.html'
     success_message = "Now you are registered, try to log in!"
+
+
+# from home page to lostitemregiste
+def tolostitemregister(request):
+    # search from database
+    # from top app get models
+    storageLocationsTag = apps.get_model('top', 'StorageLocationsTag')
+    itemsNameTag = apps.get_model('top', 'ItemsNameTag')
+    # do search
+    LocationsTags = storageLocationsTag.objects.all()
+    itemsNameTags = itemsNameTag.objects.all()
+
+    # for LocationsTag in LocationsTags:
+    #     print(LocationsTag.storage_location_id)
+
+    # back to html
+    return render(request, 'items/lost_item_register.html', {
+        'locations': LocationsTags,
+        'categories': itemsNameTags
+    })
+
+
+# click register items to save it
+def register_item(request):
+    if request.method == 'POST':
+        Item = apps.get_model('top', 'Item')
+        ItemImage = apps.get_model('top', 'ItemImage')
+
+        # item = Item.objects.create(
+        #     name=request.POST.get('name'),
+        #     description=request.POST.get('description')
+        # )
+
+        post_data = request.POST
+        color = post_data.get('color')
+        location = post_data.get('location')
+        category = post_data.get('category')
+        print(post_data)
+        # 记录这里图片取不到，可以考虑图片单独上传
+        for file in request.FILES.getlist('images'):
+            # ItemImage.objects.create(item=item, image=file)
+            print(file)
+
+        return HttpResponse(f"Location: {location}, Category: {category}, Color: {color}")
+    return HttpResponse("Please submit the form.")
