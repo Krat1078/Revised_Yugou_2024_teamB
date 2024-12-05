b3eb85818216f7d1df514bd87c2285025fb3a296
LPBin <82026062+crabin@users.noreply.github.com>
2024-12-05 15:33:02 +0900

    Top app adds a button to jump to the found item registration

--- LostItemproject/founditem/urls.py
+++ LostItemproject/founditem/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-app_name = 'lostitem'  # 定义命名空间
+app_name = 'founditem'  # 定义命名空间
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('tofounditemregister/', views.tofounditemregister, name='tofounditemregister')
 ]
