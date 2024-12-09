66fff61529f8b893d291dd9f0adb9539c60eae53
Atsuto Kurosaka <g0324058@iwate-u.ac.jp>
2024-12-09 15:24:24 +0900

    mainブランチとのマージ確認

--- LostItemproject/top/forms.py
+++ LostItemproject/top/forms.py
@@ -5,7 +5,7 @@ class TagFilterForm(forms.Form):
     itemname_tag = forms.ModelChoiceField(
         queryset=ItemsNameTag.objects.all(), 
         required=False,
-        label="落としたもの",
+        label="落とした物"
     )
     location_tag = forms.ModelChoiceField(
         queryset=PickedOrDroppedLocationsTag.objects.all(), 
