9357bfd6e31b5f25b2c9e2994cd0631e966aa0f3
RirikaYamamichi <g0324189@iwate-u.ac.jp>
2024-12-12 17:07:37 +0900

    topページ

--- LostItemproject/top/forms.py
+++ LostItemproject/top/forms.py
@@ -1,16 +1,30 @@
 from django import forms
 from .models import ItemsNameTag, PickedOrDroppedLocationsTag
 
+# class TagFilterForm(forms.Form):
+#     itemname_tag = forms.ModelChoiceField(
+#         queryset=ItemsNameTag.objects.all(), 
+#         widget=forms.Select(attrs={'class': 'form-select'}),
+#         required=False,
+#         label="落としたもの",
+#     )
+#     location_tag = forms.ModelChoiceField(
+#         queryset=PickedOrDroppedLocationsTag.objects.all(), 
+#         required=False,
+#         label="落とした場所"
+#     )
+
 class TagFilterForm(forms.Form):
     itemname_tag = forms.ModelChoiceField(
         queryset=ItemsNameTag.objects.all(), 
+        widget=forms.Select(attrs={'class': 'form-select'}),
         required=False,
-        label="落としたもの",
+        label="落としたもの"
     )
     location_tag = forms.ModelChoiceField(
-        queryset=PickedOrDroppedLocationsTag.objects.all(), 
-        required=False,
-        label="落とした場所"
+        queryset=PickedOrDroppedLocationsTag.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=False, 
     )
 
 class DateFilterForm(forms.Form):
