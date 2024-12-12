6f75f26f2f63fdd109629c54f2171de5b73721e8
LPBin <82026062+crabin@users.noreply.github.com>
2024-12-12 16:44:46 +0900

    Optimize asynchronous email sending code

--- LostItemproject/utils/email_utils.py
+++ LostItemproject/utils/email_utils.py
@@ -9,50 +9,16 @@ import base64
 
 
 # Asynchronous waiting to send mail
-@shared_task
-def send_email_async(subject, to_emails, template_name, context=None, from_email=None, attachments=None):
-    """
-        Asynchronous waiting to send mail
-        添付ファイルをサポートする HTML 電子メールを複数の受信者に送信します。
-
-        :param 件名: 電子メールの件名
-        :param to_emails: 受信者リスト (リスト)
-        :param template_name: メールコンテンツテンプレートのパス（テンプレートフォルダ下の相対パス）
-        :param context: テンプレートをレンダリングするためのコンテキスト変数 (dict)
-        :param from_email: 送信者アドレス (デフォルトは settings.DEFAULT_FROM_EMAIL)
-        :param 添付ファイル: 添付ファイルのリスト。各添付ファイルは (ファイル名、コンテンツ、MIME タイプ) 形式のタプルです。
-    """
-
-    # logger = logging.getLogger(__name__)
-    # logger.info(f"Subject: {subject}")
-    # logger.info(f"To Emails: {to_emails}")
-    # logger.info(f"Template Name: {template_name}")
-    # logger.info(f"Context: {context}")
-    # logger.info(f"Attachments: {attachments}")
-
-    if context is None:
-        context = {}
-
-    if from_email is None:
-        from_email = settings.DEFAULT_FROM_EMAIL
-
-    html_content = render_to_string(template_name, context)
-
-    email = EmailMultiAlternatives(
-        subject=subject,
-        body=html_content,
-        from_email=from_email,
-        to=to_emails
-    )
-
-    email.attach_alternative(html_content, "text/html")
-
-    if attachments and attachments != []:
-        for attachment in attachments:
-            filename, content, mimetype = attachment
-            email.attach(filename, content, mimetype)
-
-    email.send()
+@shared_task(bind=True, max_retries=3)
+def send_email_async(self, subject, to_emails, template_name, context=None, from_email=None, attachments=None):
+    logger = logging.getLogger(__name__)
+    try:
+        send_email(subject, to_emails, template_name, context, from_email, attachments)
+        logger.info(f"Email successfully sent to: {to_emails}")
+    except Exception as e:
+        logger.error(f"Failed to send email to {to_emails}: {e}")
+        logger.exception("Exception occurred while sending email")
+        raise self.retry(exc=e, countdown=60)  # 在 60 秒后重试
 
 
 # Send email directly
