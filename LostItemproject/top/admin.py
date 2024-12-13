901fd6f42c3eecf48b8eab5a51fa69e82b0a4609
Atsuto Kurosaka <g0324058@iwate-u.ac.jp>
2024-12-13 21:37:45 +0900

    Modified found object registration to register the date of registration

--- LostItemproject/top/admin.py
+++ LostItemproject/top/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import ItemsNameTag, PickedOrDroppedLocationsTag, StorageLocationsTag, Item, ItemImage
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("item_display", "created_at")
+
+    def item_display(self, obj):
+        return str(obj)
+
 # バックエンド管理への登録モデル
 admin.site.register(ItemsNameTag)
 admin.site.register(PickedOrDroppedLocationsTag)
 admin.site.register(StorageLocationsTag)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(ItemImage)
